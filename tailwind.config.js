fc5e5fac025c2f9a02493a9f1aa6602be3b7f2d5
decentparadox <rsasank69@gmail.com>
2024-01-21 21:32:08 +0530

    Added Responsiveness ig (it works in my computer idk)

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,12 @@ export default  {
       center: true,
       padding: "2rem",
       screens: {
-        "2xl": "1400px",
+        "2xl": "1536px",
+        "xl": "1280px",
+        "lg": "1024px",
+        "md": "768px",
+        "sm": "576px",
+
       },
     },
     extend: {
