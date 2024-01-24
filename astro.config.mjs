a3437028b5f257f2b734ae310cdd6f493305d14a
decentparadox <rsasank69@gmail.com>
2024-01-24 23:24:59 +0530

    Some Changes ig, Astro Icon

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,17 +3,13 @@ import tailwind from '@astrojs/tailwind';
 import react from "@astrojs/react";
 import compress from "astro-compress";
 
+import icon from "astro-icon";
+
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind({
     applyBaseStyles: false
   }),, react({
     experimentalReactChildren: true
-  }), compress({
-      CSS: true,
-			HTML: true,
-			Image: true,
-			JavaScript: true,
-			SVG: true,
-    })]
+  }), icon()]
 });
