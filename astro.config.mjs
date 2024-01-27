d9e2731a2fa1f5db844a370b659907d3c7773b78
decentparadox <rsasank69@gmail.com>
2024-01-27 14:11:20 +0530

    some changes to text

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,13 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import react from "@astrojs/react";
-import compress from "astro-compress";
-
 import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind({
     applyBaseStyles: false
-  }),, react({
+  }), react({
     experimentalReactChildren: true
   }), icon()]
 });
